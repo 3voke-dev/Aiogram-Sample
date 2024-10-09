58eddf1cc392d59dbf2a36f29729319945008df4
3voke-dev <3voke.1337@gmail.com>
2024-10-09 11:17:37 +0500

    test

--- main.py
+++ main.py
@@ -68,6 +68,6 @@ class BotApp:
 
 # Запуск бота
 if __name__ == '__main__':
-    API_TOKEN = '8152527355:AAFowmj0IYPUJNI4HB5PrVTG0yy35co1xVA'
+    API_TOKEN = 'ключ апи'
     bot_app = BotApp(token=API_TOKEN)
     asyncio.run(bot_app.run())
